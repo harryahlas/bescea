c6179a5b7b38b212eeadedabd05e86eae59903a6
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-02-15 23:48:59 -0800

    add sneapsters data

---
+++ R/sneapsters.R
@@ -0,0 +1,12 @@
+#' Selection of Text Posts
+#'
+#' A dataset containing posts from the Andy Sneap Forum. 
+#'
+#' @format A data frame with 2000 rows and 2 variables:
+#' \describe{
+#'   \item{textid}{unique id of each post}
+#'   \item{post_text}{text of the post. title has been removed.}
+#'   ...
+#' }
+#' @source \url{https://www.ultimatemetal.com/forum/forums/andy-sneap/}
+"sneapsters"
