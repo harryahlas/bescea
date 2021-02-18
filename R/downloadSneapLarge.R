92df6e061870faaeac1604cc60bafafe74d474cb
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-02-17 20:23:42 -0800

    added this though it doesn't work

---
+++ R/downloadSneapLarge.R
@@ -0,0 +1,20 @@
+#' Download Sneaplarge Data Set
+#'
+#' Load data from github. Installs into models folder.
+#' @keywords text data sneap
+#' @export
+#' @examples
+#' downloadSneapLarge()
+
+
+downloadSneaplarge <- function() {
+  download.file(url = "https://raw.githubusercontent.com/harryahlas/bescea/main/models/_fasttext_sneaplarge_.model",
+                destfile = "models/_fasttext_sneaplarge_.model")
+  
+  download.file(url = "https://raw.githubusercontent.com/harryahlas/bescea/main/models/_fasttext_sneaplarge_.model.trainables.vectors_ngrams_lockf.npy",
+                destfile = "models/_fasttext_sneaplarge_.model.trainables.vectors_ngrams_lockf.npy")
+  
+  download.file(url = "https://raw.githubusercontent.com/harryahlas/bescea/main/models/_fasttext_sneaplarge_.model.wv.vectors_ngrams.npy",
+                destfile = "models/_fasttext_sneaplarge_.model.wv.vectors_ngrams.npy")
+  
+}
