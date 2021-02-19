ee16d101539313d5bf1b61aea252e0997e6cf0a2
harryahlas <42125399+harryahlas@users.noreply.github.com>
2021-02-18 18:18:06 -0800

    cleanup to help pass check

---
+++ man/images/hex.R
@@ -0,0 +1,17 @@
+library(hexSticker)
+library(magick)
+imglocation <- "man/images/axe7.png"
+mgkimage <- image_read(imglocation)
+mgkimage_blur <- image_blur(mgkimage, 10, sigma = 1)
+
+mgkimage_blur_location <- "man/images/axe7-blur.png"
+image_write(mgkimage_blur, mgkimage_blur_location)
+
+plot(sticker(mgkimage_blur_location,
+             package="bescea", p_size=28, s_x=1, s_y=1.25, s_width=.2,
+             filename="inst/figures/imgfile.png",
+             h_color = rgb(255/255,199/255,44/255),
+             h_fill = rgb(4/255,30/255,66/255),
+             p_y = .7))
+
+save_sticker("man/images/bescea-hex.png")
